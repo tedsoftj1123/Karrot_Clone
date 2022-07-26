b15eed843f4346f0f85b589e5945cca4e170d8ec
이하성 <tedsoftj1123@gmail.com>
2022-07-26 16:24:53 +0900

    프로젝트 생성

---
+++ KarrotMarket/src/main/java/com/example/karrotmarket/KarrotMarketApplication.java
@@ -0,0 +1,13 @@
+package com.example.karrotmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KarrotMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KarrotMarketApplication.class, args);
+    }
+
+}
---
+++ KarrotMarket/src/test/java/com/example/karrotmarket/KarrotMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.karrotmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KarrotMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
