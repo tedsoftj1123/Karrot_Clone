0ae51a0b64ef01f5011c0a7e1b4c008b7d38ab16
이하성 <tedsoftj1123@gmail.com>
2022-07-27 18:35:50 +0900

    [ADD] findByEmail추가

---
+++ KarrotMarket/src/main/java/com/example/karrotmarket/repository/MemberRepository.java
@@ -0,0 +1,11 @@
+package com.example.karrotmarket.repository;
+
+import com.example.karrotmarket.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    boolean existsByMemberEmail(String email);
+    Optional<Member> findByMemberEmail(String email);
+}
