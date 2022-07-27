a973a0bd9f51ab8c40ee1dfcb2e9f7fded2a4dcb
이하성 <tedsoftj1123@gmail.com>
2022-07-27 18:35:27 +0900

    [EDIT] signatureAlgorythm 변경

--- KarrotMarket/src/main/java/com/example/karrotmarket/global/security/SecurityConfig.java
+++ KarrotMarket/src/main/java/com/example/karrotmarket/global/security/SecurityConfig.java
@@ -35,6 +35,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
 
                 .authorizeRequests()
+                .antMatchers("/auth/login").permitAll()
                 .anyRequest().permitAll()
 
                 .and()
