6944a32cd3a6170bc8e7fc15b2512a8b86ea1f9d
이하성 <tedsoftj1123@gmail.com>
2022-07-28 17:04:27 +0900

    [ADD] 마이페이지에서 진열한 물품 조회

--- KarrotMarket/src/main/java/com/example/karrotmarket/service/ItemService.java
+++ KarrotMarket/src/main/java/com/example/karrotmarket/service/ItemService.java
@@ -1,2 +1,39 @@
-package com.example.karrotmarket.service;public class ItemService {
+package com.example.karrotmarket.service;
+
+import com.example.karrotmarket.controller.dto.req.ItemRequest;
+import com.example.karrotmarket.controller.dto.res.AddItemResponse;
+import com.example.karrotmarket.domain.Item;
+import com.example.karrotmarket.domain.ItemStatus;
+import com.example.karrotmarket.domain.Member;
+import com.example.karrotmarket.facade.MemberFacade;
+import com.example.karrotmarket.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+@RequiredArgsConstructor
+public class ItemService {
+    private final ItemRepository itemRepository;
+    private final MemberFacade memberFacade;
+
+    public AddItemResponse addItem(ItemRequest req) {
+        Member member = memberFacade.getCurrentUser();
+        itemRepository.save(
+                Item.builder()
+                        .itemName(req.getItemName())
+                        .itemDescription(req.getItemDescription())
+                        .canNegotiate(req.isCanNego())
+                        .itemStatus(ItemStatus.SALE)
+                        .createdAt(LocalDateTime.now())
+                        .price(req.getItemPrice())
+                        .member(member)
+                        .build()
+        );
+        return AddItemResponse.builder()
+                .itemName(req.getItemName())
+                .memberName(member.getMemberName())
+                .build();
+    }
 }
--- KarrotMarket/src/main/java/com/example/karrotmarket/service/MemberService.java
+++ KarrotMarket/src/main/java/com/example/karrotmarket/service/MemberService.java
@@ -1,11 +1,15 @@
 package com.example.karrotmarket.service;
 
+import com.example.karrotmarket.controller.dto.res.ItemResponse;
 import com.example.karrotmarket.controller.dto.res.MyPageResponse;
 import com.example.karrotmarket.domain.Member;
 import com.example.karrotmarket.facade.MemberFacade;
 import com.example.karrotmarket.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -14,12 +18,24 @@ public class MemberService {
 
     public MyPageResponse my() {
         Member member = memberFacade.getCurrentUser();
+
+        List<ItemResponse> memberItems = member.getItems().stream()
+                .map(
+                        item -> ItemResponse.builder()
+                                .itemName(item.getItemName())
+                                .itemDescription(item.getItemDescription())
+                                .createdAt(item.getCreatedAt())
+                                .canNego(item.isCanNegotiate())
+                                .price(item.getPrice())
+                                .itemStatus(item.getItemStatus())
+                                .build()
+                ).collect(Collectors.toList());
         return MyPageResponse.builder()
                 .memberId(member.getMemberId())
                 .memberName(member.getMemberName())
                 .memberEmail(member.getMemberEmail())
                 .memberAddress(member.getAddress())
-                .memberItems(member.getItems())
+                .memberItems(memberItems)
                 .memberDealRequests(member.getDealRequests())
                 .build();
     }
