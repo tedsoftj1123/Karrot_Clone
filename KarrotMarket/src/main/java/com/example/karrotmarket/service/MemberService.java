cf541a5161afaf6642ced6ebf99048bc539abe98
이하성 <tedsoftj1123@gmail.com>
2022-08-06 20:02:48 +0900

    [EDIT] 판매한 아이템, 구매요청 분리

--- KarrotMarket/src/main/java/com/example/karrotmarket/service/MemberService.java
+++ KarrotMarket/src/main/java/com/example/karrotmarket/service/MemberService.java
@@ -1,5 +1,7 @@
 package com.example.karrotmarket.service;
 
+import com.example.karrotmarket.controller.dto.req.HandleDealRequest;
+import com.example.karrotmarket.controller.dto.req.UserDealRequest;
 import com.example.karrotmarket.controller.dto.res.DealRequestResponse;
 import com.example.karrotmarket.controller.dto.res.ItemResponse;
 import com.example.karrotmarket.controller.dto.res.MyPageResponse;
@@ -8,6 +10,7 @@ import com.example.karrotmarket.domain.Item;
 import com.example.karrotmarket.domain.ItemStatus;
 import com.example.karrotmarket.domain.Member;
 import com.example.karrotmarket.facade.MemberFacade;
+import com.example.karrotmarket.global.exception.DealRequestNotFound;
 import com.example.karrotmarket.repository.DealRequestRepository;
 import com.example.karrotmarket.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
@@ -20,12 +23,45 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class MemberService {
     private final MemberFacade memberFacade;
-
     private final DealRequestRepository dealRequestRepository;
 
     public MyPageResponse my() {
         Member member = memberFacade.getCurrentUser();
-        List<DealRequestResponse> memberDealRequests = member.getDealRequests().stream()
+        List<DealRequestResponse> acceptedDealRequests = member.getDealRequests()
+                .stream().filter(DealRequest::isAccepted)
+                .map(d -> DealRequestResponse.builder()
+                        .dealMember(d.getDealMember())
+                        .price(d.getPrice())
+                        .day(d.getDay())
+                        .location(d.getLocation())
+                        .build())
+                .collect(Collectors.toList());
+        return MyPageResponse.builder()
+                .memberId(member.getMemberId())
+                .memberName(member.getMemberName())
+                .memberEmail(member.getMemberEmail())
+                .memberAddress(member.getAddress())
+                .acceptedDealRequests(acceptedDealRequests)
+                .build();
+    }
+
+    public List<ItemResponse> userItems() {
+        Member member = memberFacade.getCurrentUser();
+        return member.getItems().stream().map(
+                item -> ItemResponse.builder()
+                        .itemName(item.getItemName())
+                        .itemDescription(item.getItemDescription())
+                        .createdAt(item.getCreatedAt())
+                        .canNego(item.isCanNegotiate())
+                        .price(item.getPrice())
+                        .itemStatus(item.getItemStatus())
+                        .build()
+        ).collect(Collectors.toList());
+    }
+
+    public List<DealRequestResponse> userDealRequests() {
+        Member member = memberFacade.getCurrentUser();
+        return member.getDealRequests().stream()
                 .map(dealRequest -> DealRequestResponse.builder()
                         .dealMember(dealRequest.getDealMember())
                         .day(dealRequest.getDay())
@@ -33,35 +69,15 @@ public class MemberService {
                         .price(dealRequest.getPrice())
                         .build()
                 ).collect(Collectors.toList());
-        List<ItemResponse> memberItems = member.getItems().stream()
-                .map(
-                        item -> ItemResponse.builder()
-                                .itemName(item.getItemName())
-                                .itemDescription(item.getItemDescription())
-                                .createdAt(item.getCreatedAt())
-                                .canNego(item.isCanNegotiate())
-                                .price(item.getPrice())
-                                .itemStatus(item.getItemStatus())
-                                .build()
-                ).collect(Collectors.toList());
-        return MyPageResponse.builder()
-                .memberId(member.getMemberId())
-                .memberName(member.getMemberName())
-                .memberEmail(member.getMemberEmail())
-                .memberAddress(member.getAddress())
-                .memberItems(memberItems)
-                .memberDealRequests(memberDealRequests)
-                .build();
     }
-
-    public void handleDealRequest(boolean accept, Long dealRequestId) {
-        DealRequest dealRequest = dealRequestRepository.findById(dealRequestId)
-                .orElseThrow(RuntimeException::new);
+    public void handleDealRequest(HandleDealRequest req) {
+        DealRequest dealRequest = dealRequestRepository.findById(req.getDealRequestId())
+                .orElseThrow(DealRequestNotFound::new);
         Item item = dealRequest.getItem();
-        if(accept) {
+        if(req.isAccept()) {
             item.changeItemStatus(ItemStatus.RESERVE);
-            return;
+            dealRequest.toAccepted();
         }
-        dealRequestRepository.deleteById(dealRequestId);
+        dealRequestRepository.deleteById(req.getDealRequestId());
     }
 }
