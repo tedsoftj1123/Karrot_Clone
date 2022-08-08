754f2253954ca03894c28a26f29d530df01b2304
이하성 <tedsoftj1123@gmail.com>
2022-08-08 19:17:48 +0900

    🐛 :: deprecated해결

--- KarrotMarket/src/main/java/com/example/karrotmarket/global/security/SecurityConfig.java
+++ KarrotMarket/src/main/java/com/example/karrotmarket/global/security/SecurityConfig.java
@@ -5,26 +5,27 @@ import com.example.karrotmarket.global.security.jwt.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
+import org.springframework.security.web.authentication.AuthenticationFilter;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig{
     private final JwtTokenProvider jwtTokenProvider;
-
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-
-    @Override
+    @Bean
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
